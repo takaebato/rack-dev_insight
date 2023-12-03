9d4f89f9e43a3f6412dad4e2f2be65ece2c1c786
Takahiro Ebato <taka2207@i.softbank.jp>
2023-12-03 16:44:52 +0900

    modified manifest json

--- extension/src/components/TraceInfo.svelte
+++ extension/src/components/TraceInfo.svelte
@@ -7,7 +7,7 @@
 
 {#if isEditorSet()}
   {@const pathInfo = buildPathInfo(traceInfo.original, traceInfo.path, traceInfo.line)}
-  <a href={pathInfo.link} target="_blank" class="hover:text-gray-500">{pathInfo.display}</a><br />
+  <a href={pathInfo.link} target="_blank" class="hover:text-gray-600">{pathInfo.display}</a><br />
 {:else}
   <p>{traceInfo.original}</p>
 {/if}
